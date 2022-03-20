0ac85918fd87ff214133a8732478472a2e8e82b8
kddoddo <yeanji_kim@naver.com>
2022-03-20 14:48:18 +0900

    Revert "noBoardController 머지하지 마세요! 머지 안되어서 커밋한고!"
    This reverts commit eaaab867bb284f00131c2d452d44ecb6b4e4519e.

--- src/main/java/com/spring/cafealley/controller/NoBoardController.java
+++ src/main/java/com/spring/cafealley/controller/NoBoardController.java
@@ -2,16 +2,10 @@ package com.spring.cafealley.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import com.spring.cafealley.board.service.IBoardService;
-import com.spring.cafealley.command.BoardVO;
-import com.spring.cafealley.util.PageCreator;
-import com.spring.cafealley.util.PageVO;
 
 @Controller
 @RequestMapping("/noBoard")
@@ -19,67 +13,32 @@ public class NoBoardController {
 
 	@Autowired
 	private IBoardService service;
-
-	//상세보기 이동
+	
 	@GetMapping("/noDetail")
 	public String nodetail() {
 		System.out.println("공지 상세보기 페이지로 이동");		
 		return "noticeboard/noticeBoardDetail";
 	}
 	
-	/*
-	// 목록 이동
 	@GetMapping("/noList")
-	public String noList() {
-		System.out.println("목록 페이지로 이동");
-		return "noticeboard/noticeBoardList";
-	}
-	*/
-	// 목록 화면
-	@GetMapping("/noList")
-	public String noList(PageVO vo, Model model) {
-		System.out.println("noList 이동");
-//		System.out.println("검색어: " + vo.getKeyword());
-//		System.out.println("검색 조건: " + vo.getCondition());
-		
-		PageCreator pc = new PageCreator();
-		System.out.println(pc);
-		pc.setPaging(vo);
-		pc.setArticleTotalCount(service.getTotal(vo));
-
-		System.out.println(pc);
-
-		model.addAttribute("boardList", service.getList(vo));
-		model.addAttribute("pc", pc);
-
+	public String nolist() {
+		System.out.println("공지 리스트 페이지로 이동");
 		return "noticeboard/noticeBoardList";
 	}
 	
-
-	//작성 이동
 	@GetMapping("/noWrite")
 	public String noWrite() {
 		System.out.println("공지 작성 페이지로 이동");
 		return "noticeboard/noticeBoardWrite";
 	}
-
-
-
-	//수정 이동
+	
+	
 	@GetMapping("/noModi")
 	public String noModi() {
 		System.out.println("공지 수정 페이지로 이동");
-		return "noticeboard/noticeBoardModi";
+		return "noticeboard/noticeBoardModify";
 	}
-
-	/*
-	@RequestMapping(value = "/noBoard/noModi", method = RequestMethod.GET)
-	public String update(@ModelAttribute("searchVO") BoardVO searchVO, @RequestParam("bno") int bno, Model model) {
-		return "/noBoard/noModi";
-	}
-	*/
-
-
-
-
+	
+	
+	
 }
