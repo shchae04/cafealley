f0436173212b31aec2c5e46ae8592a0ae5012e03
chominji98 <chominji981114@gmail.com>
2022-03-24 21:08:36 +0900

    메인 페이지에 커뮤 보드 최신글 불러오기

--- src/main/java/com/spring/cafealley/HomeController.java
+++ src/main/java/com/spring/cafealley/HomeController.java
@@ -2,16 +2,21 @@ package com.spring.cafealley;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.spring.cafealley.board.service.ICmBoardService;
+import com.spring.cafealley.command.BoardVO;
+import com.spring.cafealley.util.PageCreator;
+import com.spring.cafealley.util.PageVO;
 
 /**
  * Handles requests for the application home page.
@@ -21,25 +26,26 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
+	@Autowired
 	private ICmBoardService cmBoardService;
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
+	public String home(Locale locale, Model model, PageVO paging) {
+//		logger.info("Welcome home! The client locale is {}.", locale);
+//		
+//		Date date = new Date();
+//		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+//		
+//		String formattedDate = dateFormat.format(date);
+//		
+//		model.addAttribute("serverTime", formattedDate);
 		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		//메인에 top5 목록 불러오기
 		model.addAttribute("boardList", cmBoardService.getMainCmBaord());
-        System.out.println(cmBoardService.getMainCmBaord());
+		
 	
 		
 		return "home";
