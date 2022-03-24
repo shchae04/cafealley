81ca0cbd17b020ed1cddabaa4f770bb3f0544e9b
chominji98 <chominji981114@gmail.com>
2022-03-24 11:46:09 +0900

    IPromoboardService 오류 수정

--- src/main/java/com/spring/cafealley/promoboard/service/IPromoBoardService.java
+++ src/main/java/com/spring/cafealley/promoboard/service/IPromoBoardService.java
@@ -23,9 +23,6 @@ public interface IPromoBoardService {
 	//삭제
 	void delete(int bno);
 	
-
-	//총 게시물 수
-	int getTotal(PageVO vo);
 	
 	////////////////////////////////////////////////////////////
 	
